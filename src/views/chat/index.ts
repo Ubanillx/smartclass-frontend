a8e299ac8bf09913414844b287cd5c95ed9ff5ad
ubanillx <longxin_liu@qq.com>
2025-04-09 15:05:38 +0800

    - 统一导出格式actor导出格式并调整代码组织- 删除多余的空行和括号refviews/chat): 优化( - 调整代码缩进和格式

--- src/views/chat/index.ts
+++ src/views/chat/index.ts
@@ -3,16 +3,11 @@ import ChatDetail from './ChatDetail.vue';
 import IntelligenceCenter from './IntelligenceCenter.vue';
 import ChatContainer from './ChatContainer.vue';
 
-export {
-  ChatHistory,
-  ChatDetail,
-  IntelligenceCenter,
-  ChatContainer
-};
+export { ChatHistory, ChatDetail, IntelligenceCenter, ChatContainer };
 
 export default {
   ChatHistory,
   ChatDetail,
   IntelligenceCenter,
-  ChatContainer
-}; 
+  ChatContainer,
+};
