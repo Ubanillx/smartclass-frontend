c6094876ca4d068c632d65b36c2051a18dadaec0
Ubanillx <liulongxin21@gmail.com>
2025-05-15 21:58:06 +0800

    refactor(components): 更新组件路径并添加搜索结果组件
    - 更新 Home 组件中的 ArticleContent 引用路径
    - 新增 Search 组件，包含 ArticleResultItem、WordResultItem 和 PostResultItem 子组件

--- src/components/Home/index.ts
+++ src/components/Home/index.ts
@@ -3,7 +3,7 @@ import AiAssistantList from './AiAssistantList.vue';
 import DailyWordCard from './DailyWordCard.vue';
 import ArticleList from './ArticleList.vue';
 import PopularCourses from './PopularCourses.vue';
-import ArticleContent from './Articles/ArticleContent.vue';
+import ArticleContent from './ArticleContent.vue';
 
 export {
   NoticeCard,
---
+++ src/components/Search/index.ts
@@ -0,0 +1,9 @@
+import ArticleResultItem from './ArticleResultItem.vue';
+import WordResultItem from './WordResultItem.vue';
+import PostResultItem from './PostResultItem.vue';
+
+export {
+  ArticleResultItem,
+  WordResultItem,
+  PostResultItem
+}; 
